985be1edf2981271450d21bd010798ad2911a1bf
JakobAyo <jakob.ayo@gmail.com>
2025-05-07 02:47:22 +0200

    feat: print util with colors

---
+++ src/utils/print_utils.py
@@ -0,0 +1,16 @@
+import inquirer
+
+RED = '\x1b[0;31;40m'
+GREEN = '\x1b[0;32;40m'
+YELLOW = '\x1b[0;33;40m'
+CYAN = '\x1b[0;36;40m'
+WHITE = '\x1b[0;30;47m'
+ENDC = '\x1b[0m'
+
+def categories(categories):
+    for i, category in enumerate(categories):
+        print(YELLOW + f"{i + 1}. {category}" + ENDC)
+
+def start():
+    size = 30
+    print(WHITE + '>'*size + f"{'Suggested Categories':^25}" + '<'*size + ENDC + "\n")
